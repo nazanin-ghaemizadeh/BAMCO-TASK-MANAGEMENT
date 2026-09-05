e661491ed2d39dc89b646e3da752789421e3f4da
Nazanin <166415421+nazanin-ghaemizadeh@users.noreply.github.com>
2026-09-05 13:39:43 +0330

    Load final desktop header fixes

--- desktop-assets.js
+++ desktop-assets.js
@@ -1,1 +1,2 @@
 window.BAMCO_DESKTOP_ASSETS = window.BAMCO_DESKTOP_ASSETS || {};
+(function(){var l=document.createElement('link');l.rel='stylesheet';l.href='desktop-fixes.css';document.head.appendChild(l);})();
